f2ef8ee168f6cfa82ffa454368211f9fafdc148e
Emily Kiddell <112534759+EmKiddell@users.noreply.github.com>
2024-06-10 20:48:53 +1000

    add test.sh

---
+++ vanilla-extract-vite-plugin-test/ops/bin/test.sh
@@ -0,0 +1,9 @@
+#! /usr/bin/env bash
+set -eo pipefail
+
+imageName="v-e-plugin-test:latest"
+dockerfilePath="./Dockerfile"
+
+docker build -t ${imageName} --target test -f ${dockerfilePath} .
+
+docker run ${imageName}
