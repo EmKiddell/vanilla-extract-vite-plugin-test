0c236453f83480625b74bf4a696ab607320ec349
Emily Kiddell <112534759+EmKiddell@users.noreply.github.com>
2024-06-11 16:49:57 +1000

    chore: Update import statement in Header.tsx

--- vanilla-extract-vite-plugin-test/src/main.tsx
+++ vanilla-extract-vite-plugin-test/src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Header from './Header.tsx'
+import Header from './Header'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
